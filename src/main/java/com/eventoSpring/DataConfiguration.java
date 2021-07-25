815f055d047883d05042a68b19f1f9591f6b790d
Antonio Joaquim <a.joaquimsfilho@gmail.com>
2021-07-25 08:53:18 -0300

    config classPostgres

--- src/main/java/com/eventoSpring/DataConfiguration.java
+++ src/main/java/com/eventoSpring/DataConfiguration.java
@@ -1,60 +1,40 @@
 package com.eventoSpring;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
+//import javax.activation.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
-	// Bean para conexao no banco Mysql
-	/*
-	 * @Bean public DataSource dataSource() { DriverManagerDataSource dataSource =
-	 * new DriverManagerDataSource();
-	 * 
-	 * dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-	 * dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
-	 * dataSource.setUsername("root"); dataSource.setPassword("");
-	 * 
-	 * return dataSource;
-	 * 
-	 * }
-	 * 
-	 * @Bean public JpaVendorAdapter jpaVendorAdapter() { HibernateJpaVendorAdapter
-	 * adapter = new HibernateJpaVendorAdapter();
-	 * adapter.setDatabase(Database.MYSQL); adapter.setShowSql(true);
-	 * adapter.setGenerateDdl(true);
-	 * adapter.setDatabasePlatform("org.hibernate.dialect.MySQL55Dialect");
-	 * adapter.setPrepareConnection(true);
-	 * 
-	 * return adapter; }
-	 * 
-	 * //Bean para banco PostgresSql
-	 * 
-	 *
-	 */
-
-	@Configuration
-	// @Profile("prod")
-	public class DataConfigurationPostgreSQL {
-
-		@Bean
-		public BasicDataSource dataSource() throws URISyntaxException {
-			URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-			String username = dbUri.getUserInfo().split(":")[0];
-			String password = dbUri.getUserInfo().split(":")[1];
-			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-			BasicDataSource basicDataSource = new BasicDataSource();
-			basicDataSource.setUrl(dbUrl);
-			basicDataSource.setUsername(username);
-			basicDataSource.setPassword(password);
-
-			return basicDataSource;
-		}
-
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); 
+		dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+		dataSource.setUsername("root");
+		dataSource.setPassword("");	
+		
+		return dataSource;
+		
 	}
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL55Dialect");
+		adapter.setPrepareConnection(true);
+				
+		return adapter;
+	}
+
 }
